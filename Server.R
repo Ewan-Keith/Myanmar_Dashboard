1b79047d5bc4eba342b476fd90f920965335e3f0
Ewan Keith <keith.ewan@yahoo.co.uk>
2015-12-12 21:59:23 +0000

    Cleaned code and added extra info to downloadable data

--- Server.R
+++ Server.R
@@ -1,120 +1,339 @@
-# load necessary packages
+#### load necessary packages ####
 library(shiny) # underlies the whole application
 library(dplyr) # used for data handling
 library(tidyr) # used for tidying some data for presentation
 library(rCharts) # used for interactive charts, not hosted on CRAN so must be installed from github, see http://ramnathv.github.io/rCharts/
 library(DT) # used for interactive tables
+library(ggplot2)
+library(ggthemes)
 
-# read in data, done outside Server command for faster running
+#### Define Region/State allocation function for use later ####
+region_state <- function(raw_data){
+  
+  for(i in 1:nrow(raw_data)){
+    
+    if(raw_data[i,"region"] == "Ayeyawady"){
+      raw_data[i,"region"] <- "Ayeyawady Region"
+    }
+    
+    if(raw_data[i,"region"] == "Bago"){
+      raw_data[i,"region"] <- "Bago Region"
+    }
+    
+    
+    if(raw_data[i,"region"] == "Chin"){
+      raw_data[i,"region"] <- "Chin State"
+    }
+    
+    if(raw_data[i,"region"] == "Kachin"){
+      raw_data[i,"region"] <- "Kachin State"
+    }
+    
+    if(raw_data[i,"region"] == "Kayah"){
+      raw_data[i,"region"] <- "Kayah State"
+    }
+    
+    if(raw_data[i,"region"] == "Kayin"){
+      raw_data[i,"region"] <- "Kayin State"
+    }
+    
+    if(raw_data[i,"region"] == "Magway"){
+      raw_data[i,"region"] <- "Magway Region"
+    }
+    
+    if(raw_data[i,"region"] == "Mandalay"){
+      raw_data[i,"region"] <- "Mandalay Region"
+    }
+    
+    if(raw_data[i,"region"] == "Mon"){
+      raw_data[i,"region"] <- "Mon State"
+    }
+    
+    if(raw_data[i,"region"] == "Rakhine"){
+      raw_data[i,"region"] <- "Rakhine State"
+    }
+    
+    if(raw_data[i,"region"] == "Sagaing"){
+      raw_data[i,"region"] <- "Sagaing Region"
+    }
+    
+    if(raw_data[i,"region"] == "Shan"){
+      raw_data[i,"region"] <- "Shan State"
+    }
+    
+    if(raw_data[i,"region"] == "Tanintharyi"){
+      raw_data[i,"region"] <- "Tanintharyi Region"
+    }
+    
+    if(raw_data[i,"region"] == "Yangon"){
+      raw_data[i,"region"] <- "Yangon Region"
+    }
+    
+    
+    
+    
+  }
+  raw_data
+}
+
+#### read in and prep data ####
 full_data <- read.csv('myanmar dash data.csv', stringsAsFactors = FALSE)
 income_data <- filter(full_data, flow == 'income')
 expenditure_data <- filter(full_data, flow == 'expenditure')
 
 #### server code ####
 
-shinyServer(function(input, output) {
-  ## see the bottom of ShinyServer() function for definitions of all functions used 
+shinyServer(function(input, output, session) {
+  ## see the bottom of ShinyServer() function for definitions of all reactive functions used 
+
+#### Region Budget Summary Code ####
   
-#### Flow Breakdown Code ####
+  ## pie chart function
+  output$summary_pie <- renderChart2({
+    
+    # prep pie chart data
+    pie_data <- select(income_data, region, source, value) %>%
+      complete(region, source, fill = list(value = 0)) %>%
+      group_by(source) %>%
+      filter(region %in% input$region_summary_select) %>%
+      summarise(value = sum(value))
+    
+    # produce Rcharts pie chart
+    income_pie <- hPlot(
+      x = "source",
+      y = "value",
+      data = pie_data,
+      type = "pie",
+      title = "Region/State Income by Source",
+      subtitle = input$region_summary_select
+    )
+    
+    # set pie chart tool tip options
+    income_pie$tooltip(useHTML = T,
+                       formatter = "#! function() { return Highcharts.numberFormat(this.y,0) + 'm Kyat from ' + this.key ; } !#")
+    
+    # return pie chart object
+    income_pie
+  })
   
-  output$FB_income_chart <- renderChart2({
-    FB_income_data <- FB_data_prep()(income_data)   # see https://github.com/rstudio/shiny/issues/858 for why two sets of brackets are used
-    FB_Chart(FB_income_data, "Income")
-    })
+  ## Top ten income barchart function
+  output$top_income_items <- renderPlot({
+    
+    # prepare top ten income barchart data
+    top_10 <- income_data  %>%
+      filter(region == input$region_summary_select) %>%
+      group_by(region, budget_item)  %>%
+      summarise(value = sum(value))  %>%
+      top_n(value, n = 10)  %>%
+      arrange(desc(value))
+    
+    # produce ggplot top ten income barchart
+    top_plot <- ggplot(top_10,
+                       aes(
+                         x = reorder(budget_item, value),
+                         y = value,
+                         fill = region
+                       )) +
+      geom_bar(stat = 'identity') +
+      coord_flip() +
+      theme_hc() +
+      ggtitle(paste("Top 10 Income Budget Items in", input$region_summary_select)) +
+      scale_fill_manual(values = c("#492970", "#6789483")) +
+      labs(x = NULL, y = "Million Kyat") +
+      theme(
+        axis.text.x = element_text(colour = "black"),
+        axis.text.y = element_text(colour = "black"),
+        legend.position = "none"
+      )
+    
+    # return top ten income barchart
+    print(top_plot)
+    
+  })
   
-  output$FB_expenditure_chart <- renderChart2({
-    FB_expenditure_data <- FB_data_prep()(expenditure_data)   
-    FB_Chart(FB_expenditure_data, "Expenditure")
-    })
+  ## Top ten expenditure barchart function
+  output$top_exp_items <- renderPlot({
+    
+    # prepare top ten expenditure barchart data
+    top_10 <- expenditure_data  %>%
+      filter(region == input$region_summary_select) %>%
+      group_by(region, budget_item)  %>%
+      summarise(value = sum(value))  %>%
+      top_n(value, n = 10)  %>%
+      arrange(desc(value))
+    
+    # produce ggplot top ten expenditure barchart
+    top_plot <- ggplot(top_10,
+                       aes(
+                         x = reorder(budget_item, value),
+                         y = value,
+                         fill = region
+                       )) +
+      geom_bar(stat = 'identity') +
+      coord_flip() +
+      theme_hc() +
+      ggtitle(paste(
+        "Top 10 Expenditure Budget Items in", input$region_summary_select
+      )) +
+      scale_fill_manual(values = c("#492970", "#6789483")) +
+      labs(x = NULL, y = "Million Kyat") +
+      theme(
+        axis.text.x = element_text(colour = "black"),
+        axis.text.y = element_text(colour = "black"),
+        legend.position = "none"
+      )
+    
+    #return top ten expenditure barchart
+    print(top_plot)
+    
+  })
   
+
+#### Budget Item Code ####
   
-#### Data Table Code ####  
+  ## produce NVD3 income chart (function code at bottom of app)
+  output$item_income_chart <- renderChart2({
+    
+    # prepare NVD3 income chart data
+    item_income_data <-
+      budget_item_data_prep()(income_data)   # see https://github.com/rstudio/shiny/issues/858 for why two sets of brackets are used
+    
+    # produce NVD3 income Chart
+    budget_item_Chart(item_income_data, "Income")
+    
+  })
+  
+  ## produce NVD3 expenditure chart (function code at bottom of app)
+  output$item_expenditure_chart <- renderChart2({
+    
+    # prepare NVD3 expenditure chart data
+    item_expenditure_data <-
+      budget_item_data_prep()(expenditure_data)
+    
+    # produce NVD3 expenditure Chart
+    budget_item_Chart(item_expenditure_data, "Expenditure")
+    
+  })
+
+#### Data Table Code ####
+  
+  ## Select table data in response to user specifications
   table_data <- reactive({
     
     # switches set appropriate values for 'All' selections
     region_switched <- switch(input$table_region,
                               All = unique(as.character(full_data$region)),
-                              input$table_region
-                              )
+                              input$table_region)
     
     flow_switched <- switch(input$table_flow,
-                              All = unique(as.character(full_data$flow)),
-                              input$table_flow
-                              )
+                            All = unique(as.character(full_data$flow)),
+                            input$table_flow)
     
     item_switched <- switch(input$table_budget_item,
                             All = unique(as.character(full_data$budget_item)),
-                            input$table_budget_item
-                            )
+                            input$table_budget_item)
     
     entity_switched <- switch(input$table_entity,
                               All = unique(as.character(full_data$entity)),
-                              input$table_entity
-                              )
+                              input$table_entity)
     
     # filter data for the table based on user input
-    filter(full_data,
-                         entity %in% entity_switched,
-                         region %in% region_switched,
-                         flow %in% flow_switched,
-                         budget_item %in% item_switched
-                         )
+    temp_table_data <- filter(
+      full_data,
+      entity %in% entity_switched,
+      region %in% region_switched,
+      flow %in% flow_switched,
+      budget_item %in% item_switched
+    )
+    
+    # create identical 'data source' variable for all entries
+    temp_table_data$Data_Source <-
+      rep("2013-14 published state and region budget laws", nrow(temp_table_data))
+    
+    # add the appropriate region/state specification to each area in the tables
+    temp_table_data <- region_state(temp_table_data)
+    
+    # return table data
+    temp_table_data
   })
     
-    output$budget_table <- renderDataTable({
-      datatable(table_data(), options = list(searching = FALSE))
+  ## produce DataTable object from selected data
+  output$budget_table <- renderDataTable({
+    
+    datatable(table_data(), options = list(searching = FALSE))
+    
   })
+
+#### Download Button Code ####  
   
-  #### Download Button Code ####  
+  ## use selected table data for the appropriate download button
   output$download_table_data <- downloadHandler(
+    
     filename = 'Myanmar Budget Data (Filtered).csv',
     content = function(file) {
       write.csv(table_data(), file)
     }
   )
+    
+  ## prepare full data frame appropriate for download
   
+  # add appropriate area title (region or date)
+  full_table_data <- region_state(full_data)
+  
+  # add data source variable
+  full_table_data$Data_Source <- rep("2013-14 published state and region budget laws", nrow(full_table_data))
+  
+  ## Produce full data download button
   output$download_full_data <- downloadHandler(
     filename = 'Myanmar Budget Data (Full).csv',
     content = function(file) {
-      write.csv(full_data, file)
+      write.csv(full_table_data, file)
     }
   )
+
+#### set up reactive functions used multiple times above ####
   
-#### set up functions used multiple times above ####
-  
-  # prep data for Flow Breakdown Tab
-  FB_data_prep <- reactive({
-    function(unformatted_data){
+  ## prep data for budget_item NVD3 Barcharts
+  budget_item_data_prep <- reactive({
+    
+    # function to return formatted data
+    function(unformatted_data) {
       
-      unformatted_data %>% 
-        filter(entity == input$entity_select) %>% 
-        group_by(region, source) %>% 
+      unformatted_data %>%
+        filter(budget_item == input$item_select) %>%
+        group_by(region, source) %>%
         summarise(value = sum(value)) %>%
-        ungroup() %>% 
+        ungroup() %>%
         complete(region, source, fill = list(value = 0))
-      }
-    })
-  
-  # plot charts for Flow Breakdown Tab
-  
-  FB_Chart <- function(chart_data, Title){
-  
-    FB_rchart <- nPlot(value ~ region, group = 'source', 
-                     data = chart_data, 
-                     type = 'multiBarChart')
-  
-    FB_rchart$chart(reduceXTicks = FALSE)
-    FB_rchart$xAxis(rotateLabels=-45)
-    FB_rchart$xAxis(axisLabel = 'Region')
-    FB_rchart$templates$script <- "http://timelyportfolio.github.io/rCharts_nvd3_templates/chartWithTitle.html"
-    FB_rchart$set(title = Title)
-    FB_rchart$params$width <- 1000
-    FB_rchart$params$height <- 300
-    FB_rchart$chart(stacked = TRUE)
-  
-    return(FB_rchart)
+      
     }
+  })
   
+  ## plot NVD3 barcharts for budget_item Tab
   
+  budget_item_Chart <- function(chart_data, Title) {
+    item_rchart <- nPlot(value ~ region, group = 'source',
+                         data = chart_data,
+                         type = 'multiBarChart')
+    
+    item_rchart$chart(reduceXTicks = FALSE)
+    item_rchart$xAxis(rotateLabels = -45)
+    item_rchart$xAxis(axisLabel = 'Region')
+    item_rchart$yAxis(axisLabel = 'Kyat (Millions)', width = 61)
+    item_rchart$templates$script <-
+      "http://timelyportfolio.github.io/rCharts_nvd3_templates/chartWithTitle.html"
+    item_rchart$set(title = Title)
+    item_rchart$params$width <- 1000
+    item_rchart$params$height <- 310
+    item_rchart$chart(stacked = TRUE)
+    item_rchart$chart(margin = list(left = 75,bottom = 75))
+    
+    item_rchart$chart(tooltipContent = "#! function(key, x, y){
+                      return '<h3>' + key + '</h3>' +
+                      '<p>' + y + 'm Kyat spent in ' + x + '</p>'
+  } !#")
+    
+    return(item_rchart)
+}
   
 })
--- UI.R
+++ UI.R
@@ -1,114 +1,195 @@
+#### load necessary packages ####
 library(shiny)
 library(rCharts) # used for interactive charts, not hosted on CRAN so must be installed from github, see http://ramnathv.github.io/rCharts/
 library(dplyr) # used for data handling
+library(shinydashboard)
 library(DT) # used for interactive tables
+
+#### read and prepare data ####
 
 full_data <- read.csv('myanmar dash data.csv', stringsAsFactors = FALSE)
 income_data <- filter(full_data, flow == 'income')
 expenditure_data <- filter(full_data, flow == 'expenditure')
 
-# set income budget entity options
+#### set income budget entity options for later use ####
+
 income_budget_entity_table <- unique(income_data$entity)
+income_budget_item_table <- unique(income_data$budget_item)
 
+#### preset Disclaimer Text for use on each tab ####
 
+disclaimer <- tagList(
+  p(
+    "Figures presented are budgeted amounts published in Myanmar's 2013-14 state and region budget
+    laws. Because numbers represent budgeted amounts rather than actuals and should be interpreted with caution.",
+    class = 'disclaim'
+  ),
+  
+  p(
+    "Users interested in a more detailed treatment of the topic are directed towards",
+    a("State and Region Public Finances in Myanmar",
+      href = "http://asiafoundation.org/publications/pdf/1544"),
+    "by Dickenson-Jones, Giles, S Kanay De, and Andrea Smurra.",
+    class = 'disclaim'
+  )
+  
+)
 
-## page type has a toolbar at the top for different pages
-shinyUI(navbarPage("Myanmar Budget Dashboard", id = "nav",
-                   
-                   #### Front Page ####                       
-                   tabPanel("Front Page",
-                            
-                            div(class="outer",
-                                
-                                img(src="Myanmar_Flag.png", width="100%", height="100%"),
-                                
-                                div(class="inner",
-                                    tags$head(
-                                      includeCSS("styles.css")
-                                      )
-                                )
-                            )
-                            
-                   ),
-                   
-                   #### Regional Summary #####
-                   tabPanel("Regional Summary"
-                            
-                   ),
-                   
-                   #### Flow Breakdown #####
-                   tabPanel("Flow Breakdown",
-                            fluidPage(
-                              column(3, 
-                                     wellPanel(
-                              selectInput('entity_select', 'Select Budget Entity', income_budget_entity_table,
-                                          width = 375),
-                              
-                              "details on the charts to the right and comments on what 'entities' and 'sources' are can go here"
-                              
-                              )),
-                             
-                              column(9,
-                              fluidRow(
-                                showOutput("FB_income_chart", "nvd3")
-                              ),
-                              
-                              hr(), # this white line adds neccesary space to seperate the two plots
-                            
-                            fluidRow(
-                              showOutput("FB_expenditure_chart", "nvd3")
-                            ))
-                   )
-                   ),
-                   
-                   #### Budget Item Breakdown #####
-                   tabPanel("Budget Item Breakdown"
-                            
-                   ),
-                   
-                   #### Indepth Revenue Analysis #####
-                   tabPanel("Indepth Revenue Analysis"
-                            
-                   ),
-                   
-                   #### Data Table #####
-                   tabPanel("Data Table",
-                            
-                            fluidRow(
-                              column(4, 
-                                     selectInput("table_region", 
-                                                 "State or Region:", 
-                                                 c("All", 
-                                                   unique(as.character(full_data$region))))
-                              ),
-                              
-                              column(4, 
-                                     selectInput("table_flow", 
-                                                 "Expenditure or Revenue:", 
-                                                 c("All", 
-                                                   unique(as.character(full_data$flow))))
-                              ),
-                              column(4, 
-                                     selectInput("table_budget_item", 
-                                                 "Subnational Entity", 
-                                                 c("All", 
-                                                   unique(as.character(full_data$budget_item))))
-                              ),        
-                              column(4, 
-                                     selectInput("table_entity", 
-                                                 "Budget Category", 
-                                                 c("All",  
-                                                   unique(as.character(full_data$entity))))
-                              )        
-                            ),
-                            
-                            
-                            # add data table
-                            dataTableOutput("budget_table"),
-                            
-                            # add filtered and full download buttons
-                            downloadButton('download_table_data', 'Download table budget data'),
-                            downloadButton('download_full_data', 'Download full budget data')
-                   )
+#### setup a toolbar shiny page ####
+
+shinyUI(
+  navbarPage(
+    "Myanmar Budget Dashboard", id = "nav",
+
+#### Front Page ####
+    
+    ## set up frontpage - NOT COMPLETE AND MAY NOT BE USED IN FINAL PRODUCT
+    
+    tabPanel("Front Page",
+             
+             div(
+               class = "outer",
+               
+               img(
+                 src = "Myanmar_Flag.png", width = "100%", height = "100%"
+               ),
+               
+               div(class = "inner",
+                   tags$head(includeCSS("styles.css")))
+             )),
+
+#### Regional Summary Tab #####
+    
+    tabPanel(
+      "State and Region Budget Summary",
+      
+      column(width = 3,
+             wellPanel(
+               selectInput(
+                 'region_summary_select', 'Select Region or State to View',
+                 unique(as.character(full_data$region)),
+                 width = 375
+               ),
+               
+               p(
+                 "This page provides a summary of income sources and major
+                 expenditure items for individual states and regions for the
+                 2013-14 financial year by classification (in millions of kyat).
+                 Income source categories have been based on accounting classifications
+                 used in published budgets."
+               ),
+               
+               hr(),
+               
+               disclaimer
+               
+               )),
+      
+      column(
+        align = "center", width = 9,
+        
+        showOutput("summary_pie",  "highcharts"),
+        
+        fluidRow(box(plotOutput("top_income_items")),
+                 
+                 box(plotOutput("top_exp_items")))
+      )
+      ),
+
+#### Budget Item Breakdown #####
+    tabPanel("Budget Item Breakdown",
+             fluidPage(
+               column(3,
+                      wellPanel(
+                        selectInput(
+                          'item_select', 'Select Budget item', income_budget_item_table,
+                          width = 375
+                        ),
+                        
+                        p(
+                          "This page provides summaries of state and region budgeted expenditure
+                          and income for the 2013-14 financial year by classification (in millions of kyat)."
+                        ),
+                        
+                        hr(),
+                        
+                        disclaimer
+                        )),
+               
+               column(9,
+                      fluidRow(showOutput(
+                        "item_income_chart", "nvd3"
+                      )),
+                      
+                      
+                      br(),
+                      
+                      
+                      fluidRow(
+                        showOutput("item_expenditure_chart", "nvd3")
+                      ))
+             )),
+
+#### Data Table #####
+    tabPanel(
+      "Data Table",
+      
+      fluidRow(
+        column(4,
+               wellPanel(
+                 h5(strong("Myanmar Budget Data Table")),
+                 p(
+                   "This page allows for the 2013-14 state and region budget data to be viewed in greater detail.
+                   Data can also be downloaded in full or as specified by the chosen filters."
+                 )
+                 )),
+        
+        column(
+          4,
+          selectInput("table_region",
+                      "State or Region:",
+                      c("All",
+                        unique(
+                          as.character(full_data$region)
+                        ))),
+          
+          selectInput("table_flow",
+                      "Expenditure or Revenue:",
+                      c("All",
+                        unique(
+                          as.character(full_data$flow)
+                        )))
+        ),
+        column(
+          4,
+          selectInput("table_budget_item",
+                      "Subnational Entity",
+                      c("All",
+                        unique(
+                          as.character(full_data$budget_item)
+                        ))),
+          
+          selectInput("table_entity",
+                      "Budget Category",
+                      c("All",
+                        unique(
+                          as.character(full_data$entity)
+                        )))
+        )
+               ),
+      
+      hr(),
+      
+      # add data table
+      dataTableOutput("budget_table"),
+      
+      # add filtered and full download buttons
+      downloadButton('download_table_data', 'Download table budget data'),
+      downloadButton('download_full_data', 'Download full budget data'),
+      
+      hr(),
+      
+      disclaimer
+    )
 )                   
 )
